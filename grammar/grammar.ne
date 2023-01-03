486f1a523c1f1b94ff79671f1cb55dbf13657252
Valentin Popov <valentin.kolomensky@yandex.ru>
2023-01-04 01:55:21 +0300

    grammar: test actually asserts stuff now

--- grammar/grammar.ne
+++ grammar/grammar.ne
@@ -2,14 +2,24 @@
 @builtin "whitespace.ne"
 @builtin "number.ne"
 
-statement -> intDeclaration:+
+Sourcefile -> (IntegerDeclaration):*
 
-intDeclaration -> "octet" __ alphanumeric _ "=" _ number semicolon
-        | "word" __ alphanumeric _ "=" _ number semicolon
-        | "dword" __ alphanumeric _ "=" _ number semicolon
-        | "qword" __ alphanumeric _ "=" _ number semicolon
-        | "oword" __ alphanumeric _ "=" _ number semicolon
+Statement -> IntegerDeclaration
+
+IntegerDeclaration -> ("octet" __ alphanumeric _ "=" _ number
+        | "word" __ alphanumeric _ "=" _ number
+        | "dword" __ alphanumeric _ "=" _ number
+        | "qword" __ alphanumeric _ "=" _ number
+        | "oword" __ alphanumeric _ "=" _ number) {%
+															    function(data) {
+															    	const actual = data[0]
+															        return {
+															            type:  actual[0],
+															            name:  actual[2].join().replace(/,/g, ""),
+															            value: actual[6][0]
+															        };
+															    }
+												   %}
 
 number -> decimal | int
-semicolon -> ";"
 alphanumeric -> [a-zA-Z0-9]:+
--- grammar/test/data.test.ts
+++ grammar/test/data.test.ts
@@ -2,7 +2,6 @@ import { assertEquals } from "https://deno.land/std@0.154.0/testing/asserts.ts";
 import nearley from "npm:nearley";
 import grammar from "../.build/parser.ts";
 
-
 Deno.test({
   name: "test parsing of data types",
   fn: async (t) => {
@@ -12,13 +11,22 @@ Deno.test({
       name: "test octet",
       fn: () => {
         // TODO actually assert something
+        // URGENT
         // FIXME it seems there are many results? is the grammar ambigious?
-        parser.feed("octet i8 = 10;");
-        parser.feed("word  i16 = 10;");
-        parser.feed("dword i32 = 10;");
-        parser.feed("qword i64 = 10;");
-        parser.feed("oword i128 = 10;");
-        console.log(parser.results[0]);
+        parser.feed("octet i8 = 100");
+        parser.feed("word  i16 = 10000");
+        parser.feed("dword i32 = 100000");
+        parser.feed("qword i64 = 100000");
+        parser.feed("oword i128 = 100000");
+        assertEquals(parser.results[0], [
+          [
+            [{ type: "octet", name: "i8", value: 100 }],
+            [{ type: "word", name: "i16", value: 10000 }],
+            [{ type: "dword", name: "i32", value: 100000 }],
+            [{ type: "qword", name: "i64", value: 100000 }],
+            [{ type: "oword", name: "i128", value: 100000 }],
+          ],
+        ]);
       },
     });
   },
