c9fda9abc18cdd1cc350da4b11ba03b33a1bf527
Valentin Popov <valentin.kolomensky@yandex.ru>
2023-01-08 20:40:54 +0300

    fixed some misplacements in elf(), bytes were all around the place and readelf was freaking out

--- santa/test/elf.test.ts
+++ santa/test/elf.test.ts
@@ -1,6 +1,12 @@
 import { elf } from "../mod.ts";
 import { ELFABI, ELFMachine, ELFType, ELFConfiguration } from "../types.ts";
 
-const executable = elf({abi: ELFABI.NONE, filetype: ELFType.EXECUTABLE, machine: ELFMachine.M386});
+const executable = elf({
+	abi: ELFABI.NONE,
+	filetype: ELFType.EXECUTABLE,
+	machine: ELFMachine.M386,
+});
 
-console.log(executable)
+console.log(executable);
+
+Deno.writeFile("deno.bin", new Uint8Array(executable.binary));
